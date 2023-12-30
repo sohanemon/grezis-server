5223579b70149c4a1892a527283039f63011037c
sohanemon <m.sohanemon@gmail.com>
2023-12-30 13:00:10 +0600

    feat: Add new user validation.

---
+++ src/middleware/validate-access-token.ts
@@ -0,0 +1,22 @@
+import { RequestHandler } from 'express';
+import jwt from 'jsonwebtoken';
+
+export const validateAccessToken: RequestHandler = (req, res, next) => {
+  const accessToken =
+    req.headers.authorization?.split(' ')[1] || req.cookies.accessToken;
+
+  if (!accessToken) {
+    return res.status(401).json({ message: 'Access token not found' });
+  }
+
+  jwt.verify(
+    accessToken,
+    process.env.ACCESS_TOKEN_SECRET as string,
+    (err: unknown) => {
+      if (err) {
+        return res.status(401).json({ message: 'Invalid access token' });
+      }
+      next();
+    },
+  );
+};

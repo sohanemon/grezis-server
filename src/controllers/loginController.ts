53a4f0d9156bdbf1bb56a6af749b87af952d6aa0
sohanemon <m.sohanemon@gmail.com>
2024-01-23 15:06:03 +0600

    fix(login): Correct database query type

--- src/controllers/loginController.ts
+++ src/controllers/loginController.ts
@@ -11,7 +11,7 @@ export const loginController: RequestHandler = async (req, res) => {
   }
 
   try {
-    const user = await db.user.findUnique({
+    const user = await db.user.findFirst({
       where: { Email: email },
     });
 

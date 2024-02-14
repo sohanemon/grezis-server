3e29233dfe1a1ba06717320642be5e2260a7fd2c
sohanemon <m.sohanemon@gmail.com>
2024-02-14 23:21:14 +0600

    refactor(employeeByWorkTimeStatusController.ts): improve code formatting and remove unnecessary console.log statements

--- src/controllers/employeeByWorkTimeStatusController.ts
+++ src/controllers/employeeByWorkTimeStatusController.ts
@@ -1,51 +1,55 @@
 import { RequestHandler } from 'express';
 import { db } from '../lib/db.js';
 
-export const employeeByWorkTimeStatusController: RequestHandler = async (req: any, res) => {
+export const employeeByWorkTimeStatusController: RequestHandler = async (
+  req: any,
+  res,
+) => {
   const model = db['workTime'];
 
-  const status = (req.query.status);
-  console.log("💬 ~ constemployeeByWorkTimeStatusController:RequestHandler= ~ status:", status)
+  const status = req.query.status;
+  console.log(
+    '💬 ~ constemployeeByWorkTimeStatusController:RequestHandler= ~ status:',
+    status,
+  );
 
   const organizationId = parseInt(req.query.organizationId);
-
 
   try {
     const data = await db.employee.findMany({
       where: {
         WorkTime:
-          status === 'true' ? {
-            some: {
-              Status: null
-            }
-          } : {
-            some: {
-              Status: {
-                not: {
-                  equals: true
-                }
+          status === 'true'
+            ? {
+                some: {
+                  Status: null,
+                },
               }
-            }
-          }
-        ,
+            : {
+                some: {
+                  Status: {
+                    not: {
+                      equals: true,
+                    },
+                  },
+                },
+              },
         OrganizationId: model.fields?.OrganizationId && organizationId,
       },
-    }
-
+    });
+    console.log(
+      'result',
+      await db.employee.findMany({
+        where: {
+          WorkTime: {
+            some: {
+              Status: true,
+            },
+          },
+        },
+      }),
     );
-    console.log('result', await db.employee.findMany({
-      where: {
-        WorkTime: {
-          some: {
-            Status: true
-          }
-        }
-      }
-    }))
     return res.status(200).json(data);
-
-
-
   } catch (e) {
     console.log('🛑 ~ constdeleteData:RequestHandler= ~ e:', e);
 

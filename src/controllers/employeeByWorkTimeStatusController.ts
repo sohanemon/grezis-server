0e89608ff56e163787d920925742a442148cc174
sohanemon <m.sohanemon@gmail.com>
2024-02-14 23:20:09 +0600

    feat(schema.prisma): set default value for Status field to false feat(employeeByWorkTimeStatusController.ts): add employeeByWorkTimeStatusController to handle GET /workTime route feat(api.ts): add GET /workTime route to call employeeByWorkTimeStatusController

---
+++ src/controllers/employeeByWorkTimeStatusController.ts
@@ -0,0 +1,56 @@
+import { RequestHandler } from 'express';
+import { db } from '../lib/db.js';
+
+export const employeeByWorkTimeStatusController: RequestHandler = async (req: any, res) => {
+  const model = db['workTime'];
+
+  const status = (req.query.status);
+  console.log("💬 ~ constemployeeByWorkTimeStatusController:RequestHandler= ~ status:", status)
+
+  const organizationId = parseInt(req.query.organizationId);
+
+
+  try {
+    const data = await db.employee.findMany({
+      where: {
+        WorkTime:
+          status === 'true' ? {
+            some: {
+              Status: null
+            }
+          } : {
+            some: {
+              Status: {
+                not: {
+                  equals: true
+                }
+              }
+            }
+          }
+        ,
+        OrganizationId: model.fields?.OrganizationId && organizationId,
+      },
+    }
+
+    );
+    console.log('result', await db.employee.findMany({
+      where: {
+        WorkTime: {
+          some: {
+            Status: true
+          }
+        }
+      }
+    }))
+    return res.status(200).json(data);
+
+
+
+  } catch (e) {
+    console.log('🛑 ~ constdeleteData:RequestHandler= ~ e:', e);
+
+    return res
+      .status(500)
+      .json({ message: 'An error occurred while retrieving data', error: e });
+  }
+};
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -3,9 +3,11 @@ import { createController } from '../controllers/createController.js';
 import { deleteController } from '../controllers/deleteController.js';
 import { readController } from '../controllers/readController.js';
 import { updateController } from '../controllers/updateController.js';
+import { employeeByWorkTimeStatusController } from '../controllers/employeeByWorkTimeStatusController.js';
 
 const router = express.Router();
 
+router.get('/workTime', employeeByWorkTimeStatusController);
 router.get('/:model', readController);
 router.post('/:model', createController);
 router.patch('/:model', updateController);
